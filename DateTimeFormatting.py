8684d511c11baec78b2d0ea2d9991b04c05f8b6c
Rasool123 <rasool.pasupuleti@gmail.com>
2018-11-06 17:51:41 +0530

    Update DateTimeFormatting.py

--- DateTimeFormatting.py
+++ DateTimeFormatting.py
@@ -1,19 +1,19 @@
 # '''Datetime formatting example'''
-# import datetime
-# my_date = datetime.datetime(2018,12,29,10,30,45)
-# print(my_date)
+import datetime
+my_date = datetime.datetime(2018,12,29,10,30,45)
+print(my_date)
 #
 # ##print date in 'Month <date>, year' format. Ex December 29, 2017
-# frmt_date = '{:%B %d, %Y}'.format(my_date)
-# print(frmt_date)
+frmt_date = '{:%B %d, %Y}'.format(my_date)
+print(frmt_date)
 #
 # # print date as 'December 07, 2017 fell on <day of the week> and was <day> of the year
-# frmt_date = '{0:%B %d, %Y} fall on a {0:%A} and will be the {0:%j} of the year.'.format(my_date)
-# print(frmt_date)
+frmt_date = '{0:%B %d, %Y} fall on a {0:%A} and will be the {0:%j} of the year.'.format(my_date)
+print(frmt_date)
 
-import calendar
-year = int(input('enter a year:'))
-if calendar.isleap(year) == True:
-    print(str(year) + ' ' + 'is leap year')
-else:
-    print(str(year) + ' ' + 'is not leap year')
+#import calendar
+#year = int(input('enter a year:'))
+#if calendar.isleap(year) == True:
+#    print(str(year) + ' ' + 'is leap year')
+#else:
+#    print(str(year) + ' ' + 'is not leap year')
